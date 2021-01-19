45d76c39415cfa67890d1048c0e52af041f06421
Christian Quintero <chriswbe1993@gmail.com>
2021-01-18 20:52:00 -0500

    Semana2

--- week2.R
+++ week2.R
@@ -59,3 +59,70 @@ columnmean<-function(y, removeNA=T){
   }
   means
 }
+
+f = function(a, b){
+  a^2
+}
+f(2)
+
+myplot = function(x,y,type="l",...){ #se usa para indicar un numero variable de argumentos
+  plot(x,y,type=type,...)
+}
+
+args(paste)
+paste("a","b",sep = ":")
+
+#Alcance (Lexical scoping)
+make.power = function(n){
+  pow = function(X){
+    X^n
+  }
+  pow
+}
+cube = make.power(3)
+square = make.power(2)
+cube(3) #27
+square(3) #9
+
+y=10
+f = function(x){
+  y=2
+  y^2 + g(x)
+}
+
+g = function(x){
+  print(y)
+  x*y
+}
+f(3)
+
+#Dates and Times
+x = as.Date("1970-01-01")
+x
+unclass(x)
+unclass(as.Date("1970-01-02"))
+
+#Times in R
+x = Sys.time()
+x
+p = as.POSIXlt(x)
+names(unclass(p))
+p$sec
+
+x = Sys.time()
+x ##ya se encuentra en formato POSIXct
+unclass(x) # como la lista no posee los campos de POSIXlt, para obtener sec debe convertirse a dicho formato
+p = as.POSIXlt(x)
+p$sec
+
+datestring = c("January 10, 2012 10:40", "December 9, 2011 9:10")
+x = strptime(datestring, "%B %d, %Y %H:%M")
+x
+class(x)
+
+x = as.Date("2012-03-01")
+y = as.Date("2012-02-28")
+x-y
+x = as.POSIXct("2012-10-25 01:00:00")
+y = as.POSIXct("2012-10-25 013:00:00", tz = "GMT")
+y-x
